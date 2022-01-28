85d60040dfac6f47857dfc8276d80044c72c4e1c
nl154936 <nl154936@students.berufskolleg-borken.de>
2022-01-28 10:04:17 +0100

    icon

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+const meineApp = express()
+meineApp.set('view engine', 'ejs')
+meineApp.use(express.static('public'))
+meineApp.use(bodyParser.urlencoded({extended: true}))
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = meineApp.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
-app.get('/',(req, res, next) => {              
-    res.render('index.ejs', {})          
+meineApp.get('/',(browserAnfrage, serverAntwort, next) => {              
+    serverAntwort.render('index.ejs', {})          
 })
 
 // Wenn die login-Seite im Browser aufgerufen wird, ...
 
-app.get('/login',(req, res, next) => {              
+meineApp.get('/login',(browserAnfrage, serverAntwort, next) => {              
 
     // ... dann wird die login.ejs vom Server gerendert an den
     // Browser zurückgegeben:
 
-    res.render('login.ejs', {})          
+    serverAntwort.render('login.ejs', {})          
 })
 
 
-// Die app.post('login') wird ausgeführt, sobald der Button gedrückt wird.
+// Die meineApp.post('login') wird ausgeführt, sobald der Button gedrückt wird.
 
-app.post('/login',(req, res, next) => {              
-    res.render('index.ejs', {})          
+meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {              
+    serverAntwort.render('index.ejs', {})          
 })
