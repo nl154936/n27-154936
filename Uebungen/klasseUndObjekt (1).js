7292a27810e059c7b768252192e3278a8039cdfb
nl154936 <nl154936@students.berufskolleg-borken.de>
2022-02-11 11:00:11 +0100

    uebung 2 gemacht

--- Uebungen/klasseUndObjekt (1).js
+++ Uebungen/klasseUndObjekt (1).js
@@ -17,26 +17,64 @@ class Spieler{
 }
 
 let spielerMueller = new Spieler()
-spielerMueller.Name = "Thomas Müller"
+spielerMueller.Name = " Thomas Müller"
 spielerMueller.Nummer = 25
 spielerMueller.Position = "Stürmer"
 spielerMueller.Verein = "FCB"
+spielerMueller.Alter = 18 
 
+if(spielerMueller.Alter >= 18){
+        spielerMueller.Volljaehrig = true 
+        console.log("Der Spieler" + spielerMueller.Name + " ist volljährig.")
+
+}
+
+console.log(spielerMueller.Name)
+console.log(spielerMueller.Position)
+console.log("Der Spieler" + spielerMueller.Name + " hat die Nummer " + spielerMueller.Nummer + ".")
 
 
 
 
 
 // Übung 2
-// In einem Schulprogramm sollen Zeugnisse verwaltet werden. 
+// In einem Schulverwaltungsprogramm sollen Zeugnisse verwaltet werden. 
 // a) Identifizieren Sie das Objekt der realen Welt mit seinen relevanten Eigenschaften
 // b) Erstellen Sie die Klassendefinition
 // c) Instanzieren Sie ein Objekt der Klasse
 // d) Initialisieren Sie das Objekt mit Eigenschaftswerten
 
+// zu a) Das "Zeugnis" ist das Objekt der realen Welt. 
 
+// zu b) 
+class Zeugnis{
+    constructor(){
+        this.SchuelerName
+        this.Klasse
+        this.Geburtsdatum
+        this.Gesamtnote
+        this.Fehlstunden
+    }
+}
 
+// zu c) 
+let zuegnisPit = new Zeugnis()
+let zeugnusMax = new Zeugnis()
 
+// zu d) 
+zuegnisPit.SchuelerName = "Pit Kiff"
+zuegnisPit.Fehlstunden = 100
+zuegnisPit.Gesamtnote= 1 
+
+zeugnisMax.SchuelerName = "Max Muster"
+zeugnisMax.Fehlstunden = 10 
+zeugnusMax.Gesamtnote = 2 
+
+if(zeugnisMax.Fehlstunden > zuegnisPit.Fehlstunden){
+    console.log("Max Muster hat mehr Fehlstunden")
+}else{
+    console.log("Pit hat mehr Fehlstunden")
+}
 
 
 // Übung 3
